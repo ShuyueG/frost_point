fab95d6eeb04ae1e6e80be95012c39cb0f4a1476
Shuyue.G <frankshuyueguan@gwu.edu>
2016-07-25 16:37:33 -0400

    Update GetYearData_dopar.R

--- GetYearData_dopar.R
+++ GetYearData_dopar.R
@@ -1,11 +1,18 @@
 # the parallel computation version of code to download data for one year
 # using the "foreach" and "doParallel" packages
 # input & output: same as the GetYearData function, see in GetYearData.R
-GetYearData_dopar <- function(year, st_day, ed_day, folder) {
+
+# initialize
+  year<-2010
+  st_day<-1
+  ed_day<-365
+  folder<-"c:/data/2010/"
+  
   # need to run in the first time
-  # install.packages("foreach")
-  # install.packages("doParallel")
-  # install.packages("RCurl")
+  install.packages("foreach")
+  install.packages("doParallel")
+  install.packages("RCurl")
+  
   source("Extract_GRIB.R")
   # time list for one day
   time <- c("00", "03", "06", "09", "12", "15", "18", "21")
@@ -30,4 +37,3 @@ GetYearData_dopar <- function(year, st_day, ed_day, folder) {
   foreach(d = st_day:ed_day) %dopar% func(d)
   stopCluster(cl) # ending
   return("Done.")
-}
